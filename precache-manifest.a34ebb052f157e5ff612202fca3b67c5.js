6c7830d4584e700ebfb5f48455c8c18f4c3094cf
Pratheesh <58771724+pappater@users.noreply.github.com>
2025-09-13 21:11:37 +0000

    Deploy to GitHub Pages

---
+++ precache-manifest.a34ebb052f157e5ff612202fca3b67c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ae00f8b9485d482e70a34943d2f74e5",
+    "url": "/yearinprogress-react/index.html"
+  },
+  {
+    "revision": "c71207e37a5e36106fbc",
+    "url": "/yearinprogress-react/static/css/2.35f98baf.chunk.css"
+  },
+  {
+    "revision": "c8277885d9f78e398d76",
+    "url": "/yearinprogress-react/static/css/main.17f93193.chunk.css"
+  },
+  {
+    "revision": "c71207e37a5e36106fbc",
+    "url": "/yearinprogress-react/static/js/2.e1d12a69.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/yearinprogress-react/static/js/2.e1d12a69.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c8277885d9f78e398d76",
+    "url": "/yearinprogress-react/static/js/main.3dc3a02f.chunk.js"
+  },
+  {
+    "revision": "48c0db809b218ee9ea31",
+    "url": "/yearinprogress-react/static/js/runtime-main.bc12a849.js"
+  }
+]);
